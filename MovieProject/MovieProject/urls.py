ce4021c8b33a4cdeab4a10dee26f825a0d1a59c1
AnandKorangle <anandsuralkar36@gmail.com>
2024-04-10 16:53:04 +0530

    Integrate Angular build into django

--- MovieProject/MovieProject/urls.py
+++ MovieProject/MovieProject/urls.py
@@ -1,24 +1,14 @@
-"""MovieProject URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path,re_path as url
 from django.conf.urls import include
 from MovieApp import urls,views
+from django.views.generic import TemplateView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^',include(urls))
+    path('', TemplateView.as_view(template_name='index.html')),  # Serve index.html for the root URL
+    path('', include(urls)),  # Include MovieApp URLs
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
